e661ea4cfcf28a2b2808185aa5310789c9917532
Ankush1oo8 <154556772+Ankush1oo8@users.noreply.github.com>
2025-02-11 19:15:58 +0530

    sw reg

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 let deferredPrompt;
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js').then(reg => {
+  navigator.serviceWorker.register('../sw.js').then(reg => {
     console.log('✅ Service Worker registered:', reg);
   }).catch(err => console.log('❌ Service Worker registration failed:', err));
 }
